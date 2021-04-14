2044a2066b548036c009f97c690fe942d6f79859
Ed Clark <clarked@microsoft.com>
2021-04-14 14:13:33 -0400

    Bind cosmosdb output

--- TransactionsETL/Function1.cs
+++ TransactionsETL/Function1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.WebJobs;
@@ -20,35 +21,17 @@ namespace TransactionsETL
         }
 
         [FunctionName("Function1")]
-        public static async Task<string> Run([BlobTrigger("transactions/{name}", Connection = "storageConnectionString")]Stream myBlob, string name, ILogger log)
+        public static async void Run([BlobTrigger("transactions/{name}", Connection = "storageConnectionString")]Stream myBlob,
+            [CosmosDB(
+    databaseName: "finvisdb",
+    collectionName: "transactions",
+    ConnectionStringSetting = "cosmosdbConnectionString")]ICollector<dynamic> documentsOut,
+            string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
 
-            string docdbConnectionString = Environment.GetEnvironmentVariable("cosmosdbConnectionString");
-            if (docdbConnectionString == null)
-            {
-                throw new Exception("Missing DocumentDB connection string");
-            }
-
-            using (DocumentClient client = new DocumentClient(new Uri(docdbConnectionString), (string)null)) {
-                Uri collectionLInk = UriFactory.CreateDocumentCollectionUri("db_id", "col_id");
-                ResourceResponse<DocumentCollection> docCollectionRep = await client.ReadDocumentCollectionAsync(collectionLInk);
-                if (docCollectionRep.StatusCode != System.Net.HttpStatusCode.OK)
-                {
-                    throw new Exception("Error getting collection link: " + docCollectionRep);
-                }
-
-                Transaction t = new Transaction{ TransactionId = "foo" };
-
-                ResourceResponse<Document> docRep = await client.CreateDocumentAsync(collectionLInk, t);
-                    
-                if (docRep.StatusCode != System.Net.HttpStatusCode.OK)
-                {
-                    throw new Exception("Error creating document: " + docRep);
-                }
-            }
-
-            return "good";
+            Transaction t = new Transaction { TransactionId = name };
+            documentsOut.Add(t);
         }
     }
 }
