8e5e40025c3309af25d8f03cd49280b7e08d194f
Ed Clark <clarked@microsoft.com>
2021-04-14 13:55:19 -0400

    Make TransactionsETL async

--- TransactionsETL/Function1.cs
+++ TransactionsETL/Function1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.WebJobs;
@@ -19,15 +20,16 @@ namespace TransactionsETL
         }
 
         [FunctionName("Function1")]
-        public static async void Run([BlobTrigger("transactions/{name}", Connection = "storageConnectionString")]Stream myBlob, string name, ILogger log)
+        public static async Task<string> Run([BlobTrigger("transactions/{name}", Connection = "storageConnectionString")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
 
             string docdbConnectionString = Environment.GetEnvironmentVariable("cosmosdbConnectionString");
-            if(docdbConnectionString == null)
+            if (docdbConnectionString == null)
             {
                 throw new Exception("Missing DocumentDB connection string");
             }
+
             using (DocumentClient client = new DocumentClient(new Uri(docdbConnectionString), (string)null)) {
                 Uri collectionLInk = UriFactory.CreateDocumentCollectionUri("db_id", "col_id");
                 ResourceResponse<DocumentCollection> docCollectionRep = await client.ReadDocumentCollectionAsync(collectionLInk);
@@ -39,11 +41,14 @@ namespace TransactionsETL
                 Transaction t = new Transaction{ TransactionId = "foo" };
 
                 ResourceResponse<Document> docRep = await client.CreateDocumentAsync(collectionLInk, t);
+                    
                 if (docRep.StatusCode != System.Net.HttpStatusCode.OK)
                 {
                     throw new Exception("Error creating document: " + docRep);
                 }
             }
+
+            return "good";
         }
     }
 }
