89bef89adbf64a8a370b6d8c4bc301bb6cea7ce8
Ed Clark <ebclark2@gmail.com>
2021-05-03 12:32:22 -0400

    Add transform code

---
+++ TransactionsETL/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+
+namespace FinVis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InfluxDBClient influxDBClient = InfluxDBClientFactory.Create("http://localhost:8086", "test");
+
+            Source source = new BOASource("C:\\Users\\B\\Downloads\\ExportData.csv");
+            using (var writeApi = influxDBClient.GetWriteApi())
+            {
+                foreach (Transaction t in source)
+                {
+                    t.Date = TimeZoneInfo.ConvertTimeToUtc(t.Date);
+//                    Console.WriteLine(t);
+
+                    writeApi.WriteMeasurement("bucket", "org", WritePrecision.Ns, t);
+                }
+            }
+        }
+    }
+}
---
+++ TransactionsETL/Source.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using InfluxDB.Client.Core;
+
+
+namespace FinVis
+{
+    public enum Status
+    {
+        Pending,
+        Posted
+    }
+
+    [Measurement("transaction")]
+    public class Transaction {
+        [Column("value")] public string Status { get; set; }
+        [Column(IsTimestamp = true)] public DateTime Date { get; set; }
+        [Column("value")] public string Description { get; set; }
+        [Column("value")] public string Category { get; set; }
+        [Column("value")] public double Amount { get; set; }
+        [Column("value")] public string Account { get; set; }
+
+        public override string ToString()
+        {
+            return "[status=" + this.Status + ", date=" + this.Date + ", description=" + this.Description + ", category=" + this.Category + ", amount=" + this.Amount + ", account=" + this.Account + "]";
+        }
+    }
+
+    interface Source: IEnumerable<Transaction>
+    {
+    }
+}
