a33ef554c225bb19812d754889b6ca890050cbe3
Ed Clark <clarked@microsoft.com>
2021-05-04 08:17:13 -0400

    Add common lib project

---
+++ FinVisCommon/BOASource.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace FinVis
+{
+
+    class BOASource : Source
+    {
+        private static Dictionary<string, string> _headerMapping = new Dictionary<string, string>()
+        {
+            //{ "Status", "bar" },
+            //{ "Date", "bar" },
+            { "Original Description", "Description" },
+            //{ "Category", "bar" },
+            //{ "Amount", "bar" },
+            { "Account Name", "Account" }
+        };
+
+        private CsvReader _csvReader;
+        private IEnumerable<Transaction> _transactions;
+
+        public BOASource(string filename)
+        {
+            this.LoadFile(filename);
+        }
+
+        public IEnumerator<Transaction> GetEnumerator()
+        {
+            return this._transactions.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this._transactions.GetEnumerator();
+        }
+
+        private void LoadFile(string filename)
+        {
+            CsvConfiguration csvConfig = new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture)
+            {
+                PrepareHeaderForMatch = args => _headerMapping.GetValueOrDefault(args.Header, args.Header).Trim()
+            };
+            TextReader reader = new StreamReader(filename);
+            _csvReader = new CsvReader(reader, csvConfig);
+            _transactions = _csvReader.GetRecords<Transaction>();
+        }
+
+    }
+}
---
+++ FinVisCommon/Source.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FinVis
+{
+    public enum Status
+    {
+        Pending,
+        Posted
+    }
+
+    public class Transaction {
+        public string Status { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public double Amount { get; set; }
+        public string Account { get; set; }
+
+        public override string ToString()
+        {
+            return "[status=" + this.Status + ", date=" + this.Date + ", description=" + this.Description + ", category=" + this.Category + ", amount=" + this.Amount + ", account=" + this.Account + "]";
+        }
+    }
+
+    public interface Source: IEnumerable<Transaction>
+    {
+    }
+}
